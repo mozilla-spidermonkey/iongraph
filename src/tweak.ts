a5c44a62cc28d5b17759e4c8284d89b791b4be67
Ben Visness <bvisness@mozilla.com>
2025-08-26 13:18:39 -0500

    Update tweaks.ts

--- src/tweak.ts
+++ src/tweak.ts
@@ -1,42 +1,7 @@
 /**
  * A Tweak is a "mutable number"—a value that coerces to a number in most
  * contexts but can be mutated at runtime to allow for easy experimentation.
- * Tweaks are automatically registered into a global {@link Tweaks} object and
- * given UI for changing its value. The experience is inspired by
- * [Dear ImGui](https://github.com/ocornut/imgui).
- *
- * To create a Tweak, call the {@link tweak} function:
- *
- *     const TWEAKY_CONSTANT = tweak("Tweak Name", 3);
- *
- * This will create a Tweak with an initial value of 3, which can be used
- * anywhere a numeric constant would be used. Other options are available via
- * an optional third parameter, such as the ability to control the min and max
- * values or to register the Tweak with a different Tweaks object. See
- * {@link TweakOptions} for details.
- *
- * Two more steps are required to set your project up for tweaks:
- *
- * 1. Add the Tweaks UI to your page:
- *
- *        document.querySelector("body").appendChild(window.tweaks.container);
- *
- * 2. Listen for changes to Tweaks and refresh your app accordingly:
- *
- *        window.addEventListener("tweak", () => {
- *          updateAll();
- *        });
- *
- * Tweaks use `valueOf` and `Symbol.toPrimitive` so that they behave like
- * numbers when used in expressions. We do some TypeScript shenanigans to allow
- * the Tweak type to be used anywhere numbers are used. However, one big
- * annoyance is that exotic objects are always truthy, and the
- * [ToBoolean](https://262.ecma-international.org/8.0/#sec-toboolean) operation
- * doesn't allow for any hooks to modify this behavior. Attempting to use a
- * Tweak as a boolean value will therefore not work as expected. The easiest
- * workaround is to put a unary plus in front of the Tweak when using it, e.g.
- * `if (+DEBUG)`, in order to explicitly convert it to a number before
- * assessing its truthiness.
+ * For more information, see the {@link tweak} function.
  */
 export type Tweak = number & _Tweak;
 interface _Tweak {
@@ -78,8 +43,46 @@ export interface TweakOptions {
 export type OnTweakChangeFn = (newValue: number) => void;
 
 /**
- * Creates a new Tweak. See the documentation of the {@link Tweak} type
- * for details.
+ * Creates a `Tweak` and adds UI for it to be modified.
+ *
+ * A Tweak is a "mutable number"—a value that coerces to a number in most
+ * contexts but can be mutated at runtime to allow for easy experimentation.
+ * Tweaks are automatically registered into a global {@link Tweaks} object and
+ * given UI for changing its value. The experience is inspired by
+ * [Dear ImGui](https://github.com/ocornut/imgui).
+ *
+ * A Tweak can be created using the {@link tweak} function:
+ *
+ *     const TWEAKY_CONSTANT = tweak("Tweak Name", 3);
+ *
+ * This will create a Tweak with an initial value of 3, which can be used
+ * anywhere a numeric constant would be used. Other options are available via
+ * an optional third parameter, such as the ability to control the min and max
+ * values or to register the Tweak with a different Tweaks object. See
+ * {@link TweakOptions} for details.
+ *
+ * Two more steps are required to set your project up for tweaks:
+ *
+ * 1. Add the Tweaks UI to your page:
+ *
+ *        document.querySelector("body").appendChild(window.tweaks.container);
+ *
+ * 2. Listen for changes to Tweaks and refresh your app accordingly:
+ *
+ *        window.addEventListener("tweak", () => {
+ *          updateAll();
+ *        });
+ *
+ * Tweaks use `valueOf` and `Symbol.toPrimitive` so that they behave like
+ * numbers when used in expressions. We do some TypeScript shenanigans to allow
+ * the Tweak type to be used anywhere numbers are used. However, one big
+ * annoyance is that exotic objects are always truthy, and the
+ * [ToBoolean](https://262.ecma-international.org/8.0/#sec-toboolean) operation
+ * doesn't allow for any hooks to modify this behavior. Attempting to use a
+ * Tweak as a boolean value will therefore not work as expected. The easiest
+ * workaround is to put a unary plus in front of the Tweak when using it, e.g.
+ * `if (+DEBUG)`, in order to explicitly convert it to a number before
+ * assessing its truthiness.
  */
 export function tweak(name: string, initial: number, options: TweakOptions = {}): Tweak {
   let value = initial;
